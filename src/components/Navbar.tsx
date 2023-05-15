e07be7b9fcf4b4495020cbe2d39a3eb18a801052
AdiMunawar31 <munawar.adi31@gmail.com>
2023-05-15 13:14:25 +0700

    change svg with react icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import {
+  IoIosArrowRoundBack,
+  IoMdCheckmarkCircleOutline,
+  IoMdMail,
+  IoMdStar,
+  IoMdStarOutline,
+  IoMdTrash,
+  IoMdWarning,
+} from "react-icons/io";
+import { BiArchiveIn, BiTrashAlt } from "react-icons/bi";
+import { AiOutlineMail, AiOutlineStar } from "react-icons/ai";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div>
       <nav aria-label="top bar" className="flex-none flex justify-between bg-white h-12">
         {/* top bar left */}
-        <ul aria-label="top bar left" aria-orientation="horizontal" className="flex">
+        <ul aria-label="top bar left" className="flex">
           {/* add button */}
           <li className="group relative">
             <div className="flex items-center h-full px-4 text-sm">
@@ -14,13 +26,7 @@ const Navbar = () => {
                 className="flex items-center text-gray-700 px-2 py-1 space-x-0.5 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                 title="Back"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path
-                    fillRule="evenodd"
-                    d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <IoIosArrowRoundBack />
                 <span className="text-sm font-bold">Back</span>
               </a>
               <div className="flex items-center">
@@ -30,58 +36,19 @@ const Navbar = () => {
                     title="Archive"
                     className="text-gray-700 px-2 py-1 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20"
-                      />
-                    </svg>
+                    <BiArchiveIn className="h-5 w-5" />
                   </button>
                   <button
                     title="Mark As Spam"
                     className="text-gray-700 px-2 py-1 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                      />
-                    </svg>
+                    <IoMdWarning className="h-5 w-5" />
                   </button>
                   <button
                     title="Delete"
                     className="text-gray-700 px-2 py-1 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                      />
-                    </svg>
+                    <BiTrashAlt className="h-5 w-5" />
                   </button>
                 </div>
                 <span className="bg-gray-300 h-6 w-[.5px] mx-3" />
@@ -90,202 +57,21 @@ const Navbar = () => {
                     title="Mark As Unread"
                     className="text-gray-700 px-2 py-1 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                      />
-                    </svg>
+                    <AiOutlineMail className="h-5 w-5" />
                   </button>
                   <button
                     title="Add Star"
                     className="text-gray-700 px-2 py-1 border border-gray-300 rounded-lg shadow hover:bg-gray-200 transition duration-100"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                      />
-                    </svg>
+                    <AiOutlineStar className="h-5 w-5" />
                   </button>
                 </div>
               </div>
             </div>
-            <span className="absolute p-1 hidden group-hover:block">
-              <ul
-                id="add"
-                role="listbox"
-                className="outline-none py-2 bg-white border rounded-md w-screen max-w-md w-dropdown-large shadow-lg focus:outline-none leading-relaxed"
-              >
-                <li role="separator" className="mb-2">
-                  <label className="block px-4 py-3 font-semibold">New</label>
-                  <hr />
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  Ticket
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  User
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  Organization
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  Search
-                </li>
-                <li role="separator" className="mb-2">
-                  <label className="block px-4 py-3 font-semibold">Recently Viewed</label>
-                  <hr />
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  <div className="flex">
-                    <div className="pr-2">
-                      <span
-                        style={{ padding: "2px 5px", fontSize: "0.7rem" }}
-                        className="font-mono rounded-sm bg-red-600 text-white leading-none"
-                      >
-                        O
-                      </span>
-                    </div>
-                    <div className="flex-1">
-                      <p>Vertias - ams opps issue</p>
-                      <p className="text-gray-600">
-                        <span>#ticket/14352</span>
-                        <span className="mx-1 font-black">·</span>
-                        <span>Nico Braun</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  <div className="flex">
-                    <div className="pr-2">
-                      <span
-                        style={{ padding: "2px 5px", fontSize: "0.7rem" }}
-                        className="font-mono rounded-sm bg-yellow-400 text-black leading-none"
-                      >
-                        N
-                      </span>
-                    </div>
-                    <div className="flex-1">
-                      <p>Vertias - ams opps issue</p>
-                      <p className="text-gray-600">
-                        <span>#ticket/14352</span>
-                        <span className="mx-1 font-black">·</span>
-                        <span>Nico Braun</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  <div className="flex">
-                    <div className="pr-2">
-                      <span
-                        style={{ padding: "2px 5px", fontSize: "0.7rem" }}
-                        className="font-mono rounded-sm bg-gray-500 text-white leading-none"
-                      >
-                        S
-                      </span>
-                    </div>
-                    <div className="flex-1">
-                      <p>Vertias - ams opps issue</p>
-                      <p className="text-gray-600">
-                        <span>#ticket/14352</span>
-                        <span className="mx-1 font-black">·</span>
-                        <span>Nico Braun</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  <div className="flex">
-                    <div className="pr-2">
-                      <span
-                        style={{ padding: "2px 5px", fontSize: "0.7rem" }}
-                        className="font-mono rounded-sm bg-blue-600 text-white leading-none"
-                      >
-                        P
-                      </span>
-                    </div>
-                    <div className="flex-1">
-                      <p>Vertias - ams opps issue</p>
-                      <p className="text-gray-600">
-                        <span>#ticket/14352</span>
-                        <span className="mx-1 font-black">·</span>
-                        <span>Nico Braun</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li
-                  role="option"
-                  className="px-6 py-1 my-1 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 cursor-pointer"
-                >
-                  <div className="flex">
-                    <div className="pr-2">
-                      <span
-                        style={{ padding: "2px 5px", fontSize: "0.7rem" }}
-                        className="font-mono rounded-sm bg-gray-800 text-white leading-none"
-                      >
-                        H
-                      </span>
-                    </div>
-                    <div className="flex-1">
-                      <p>Vertias - ams opps issue</p>
-                      <p className="text-gray-600">
-                        <span>#ticket/14352</span>
-                        <span className="mx-1 font-black">·</span>
-                        <span>Nico Braun</span>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </span>
           </li>
         </ul>
         {/* to bar right  */}
-        <ul aria-label="top bar right" aria-orientation="horizontal" className="px-8 flex items-center">
+        <ul aria-label="top bar right" className="px-8 flex items-center">
           <li className="relative">
             <input
               title="Search Bar"
@@ -370,7 +156,10 @@ const Navbar = () => {
               aria-label="page menu"
               className="h-full w-full rounded-full border focus:outline-none focus:shadow-outline"
             >
-              <img
+              <Image
+                width={30}
+                height={30}
+                alt="Avatar"
                 className="h-full w-full rounded-full mx-auto"
                 src="https://raw.githubusercontent.com/bluebrown/tailwind-zendesk-clone/master/public/assets/me.jpg"
               />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,182 +1,68 @@
+import Image from "next/image";
 import React from "react";
+import { AiFillDashboard } from "react-icons/ai";
+import { GoSignOut } from "react-icons/go";
+import { FaBlogger, FaUsers } from "react-icons/fa";
+import { MdForum } from "react-icons/md";
 
 const Sidebar = () => {
   return (
     <div className="col-span-2 px-4">
-      <div className="px-2 pt-4 pb-8 border-r min-h-full border-gray-300">
+      <div className="px-2 pt-4 -ml-2 pb-8 border-r min-h-full border-gray-300">
         <div className="mb-8 mt-2 -ml-4">
           <h1 className="font-bold text-4xl text-center md:hidden">
-            D<span className="text-teal-600">.</span>
+            D<span className="text-gray-600">.</span>
           </h1>
           <h1 className="hidden md:block font-bold text-sm md:text-xl text-center ml-4">
-            D2Y OFFICIAL<span className="text-teal-600">.</span>
+            D2Y OFFICIAL<span className="text-gray-600">.</span>
           </h1>
           <div id="profile" className="space-y-3">
-            <img src="/images/am.jpg" alt="Avatar user" className="w-10 md:w-16 rounded-full mx-auto" />
+            <Image
+              src="/images/am.jpg"
+              width="50"
+              height="50"
+              alt="Avatar user"
+              className="w-10 md:w-16 rounded-full mx-auto animate-pulse transition-all"
+            />
             <div>
-              <h2 className="font-medium text-xs md:text-sm text-center text-teal-500">Adi Munawar</h2>
+              <h2 className="font-medium text-xs md:text-sm text-center text-gray-800">Adi Munawar</h2>
               <p className="text-xs text-gray-500 text-center">Administrator</p>
             </div>
           </div>
         </div>
 
-        <ul className="space-y-2">
+        <ul className="space-y-2 mr-2">
           <li>
-            <a className="bg-gray-500 bg-opacity-30 text-blue-600 flex items-center justify-between py-1.5 px-4 rounded cursor-pointer">
+            <a className="bg-black text-gray-100 flex items-center justify-between py-1.5 px-4 rounded cursor-pointer">
               <span className="flex items-center space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                  />
-                </svg>
-                <span>Inbox</span>
+                <AiFillDashboard />
+                <span>Dashboard</span>
               </span>
-              <span className="bg-sky-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">3</span>
+              <span className="bg-red-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">3</span>
             </a>
           </li>
           <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
-              <span>Starred</span>
+            <a className="hover:bg-black hover:text-gray-100 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+              <FaUsers />
+              <span>Users</span>
             </a>
           </li>
           <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span>Snoozed</span>
+            <a className="hover:bg-black hover:text-gray-100 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+              <FaBlogger />
+              <span>Blogs</span>
             </a>
           </li>
           <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-                />
-              </svg>
-              <span>Important</span>
+            <a className="hover:bg-black hover:text-gray-100 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+              <MdForum />
+              <span>Forums</span>
             </a>
           </li>
           <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 rotate-90"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                />
-              </svg>
-              <span>Sent</span>
-            </a>
-          </li>
-          <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center justify-between text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <span className="flex items-center space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                  />
-                </svg>
-                <span>Drafts</span>
-              </span>
-              <span className="bg-sky-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">1</span>
-            </a>
-          </li>
-          <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                />
-              </svg>
-              <span>Spam</span>
-            </a>
-          </li>
-          <li>
-            <a className="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-              <span>Trash</span>
+            <a className="hover:bg-black hover:text-gray-100 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+              <GoSignOut />
+              <span>Logout</span>
             </a>
           </li>
         </ul>
